a93e7fff2d02bbb3e4fbf1527afd900cb246b537
noor Shalayel <nooresam2001@gmail.com>
2022-07-31 14:48:57 +0300

    up

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const config = require('./config');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const eventRoutes = require('./routes/route');
 
 const app = express();
 
 app.use(express.json());  
 app.use(cors());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 app.use('/api', eventRoutes.routes);
 
